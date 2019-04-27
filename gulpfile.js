b7a8a35915329aaea2d78a0ca6ee074c07fdafbe
Abdulla Shaikh <Shaikhabdul9990@gmail.com>
2019-04-27 18:24:28 +0530

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');  
+
+
+
+function style(){
+    return gulp.src(['src/scss/*.scss'])
+    .pipe(sass())
+    .pipe(gulp.dest('src/css'))
+    .pipe(browserSync.stream());
+}
+
+function watch(){
+    browserSync.init({
+        server:{
+            baseDir:'./src/'
+          }
+    });
+    gulp.watch('src/scss/**/*.scss',style);
+  gulp.watch('src/*.html').on('change', browserSync.reload);
+  gulp.watch('src/js/**/*.js').on('change', browserSync.reload); 
+}
+
+
+exports.style = style;
+exports.watch = watch;
+
+
+// Default Task
+gulp.task('default', exports.watch = watch);
